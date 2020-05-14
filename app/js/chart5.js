152e1da8112044a969021095fef370471b320069
zjhch123 <416836508@qq.com>
2020-05-14 20:16:02 +0800

    comment

--- app/js/chart5.js
+++ app/js/chart5.js
@@ -8,11 +8,19 @@ function renderChart5() {
     tooltip: {
       trigger: 'axis',
       axisPointer: {
-        type: 'shadow'
+        type: 'shadow',
+        snap: false,
       },
       formatter: ([{ dataIndex }]) => {
-        return `${Data_RumourTop20[dataIndex][1]}`
+        return `${Data_RumourTop20[dataIndex][1]}<br>浏览量 : ${Data_RumourTop20[dataIndex][2]}<br>评论量 : ${Data_RumourTop20[dataIndex][3]}`
       },
+      textStyle: {
+        fontFamily: 'gbk',
+      }
+    },
+    legend: {
+      data: ['view', 'comment'],
+      show: false,
     },
     grid: {
       containLabel: true,
@@ -21,13 +29,20 @@ function renderChart5() {
       bottom: '0',
       right: 80,
     },
-    xAxis: {
+    xAxis: [{
       splitLine: {show: false},
       axisLabel: {show: false},
       axisTick: {show: false},
       axisLine: {show: false},
-    },
-    yAxis: {
+    }, {
+      splitLine: {show: false},
+      axisLabel: {show: false},
+      axisTick: {show: false},
+      axisLine: {show: false},
+      min: 0,
+      max: 5000,
+    }],
+    yAxis: [{
       data: Data_RumourTop20.slice(0, 5).map((row, index) => ``),
       inverse: true,
       axisLine: {show: false},
@@ -43,14 +58,35 @@ function renderChart5() {
           margin: 30
         }
       }
-    },
+    }],
     series: [{
+      name: 'view',
       type: 'pictorialBar',
       symbol: Data_Pic,
       symbolRepeat: true,
-      symbolMargin: '5%',
-      symbolSize: 40,
+      symbolMargin: '8%',
+      symbolSize: 20,
       data: Data_RumourTop20.slice(0, 5).map(data => Number(data[2])),
+      label: {
+        normal: {
+          show: true,
+          position: 'right',
+          offset: [10, 0],
+          textStyle: {
+            fontSize: 12
+          }
+        }
+      }
+    }, {
+      name: 'comment',
+      type: 'pictorialBar',
+      symbol: Data_Comment_Pic,
+      symbolRepeat: true,
+      symbolMargin: '8%',
+      symbolSize: 20,
+      barGap: '10%',
+      data: Data_RumourTop20.slice(0, 5).map(data => Number(data[3])),
+      xAxisIndex: 1,
       label: {
         normal: {
           show: true,
